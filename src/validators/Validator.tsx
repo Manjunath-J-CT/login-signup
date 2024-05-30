32c7ef6a0d2d5e25ffbfd91d9e0c5246458072b0
Manjunath-J-CT <manjunath.j@codeandtheory.com>
2024-05-30 12:53:07 +0530

    Features/login (#8)
    * [Manju]refactor Login
    
    * [Manju]created validator
    
    * [Manju]:refactor login

--- src/validators/Validator.tsx
+++ src/validators/Validator.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 
-export const Validator = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { z } from "zod";
 
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .nonempty({ message: "Email is required" })
+    .email({ message: "Email is not valid" }),
+  password: z
+    .string()
+    .nonempty({ message: "Password is required" })
+    .min(8, { message: "Requires at least 8 characters" }),
+});
